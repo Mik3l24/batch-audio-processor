d7995c028cc276285f56d8d0d34b4bf2e78dc6f6
Mik3l24 <michalczus@gmail.com>
2023-06-08 21:09:27 +0200

    Added loudness functionality and loading of audio data

--- fileinfo.py
+++ fileinfo.py
@@ -1,7 +1,14 @@
+import io
 from typing import Optional
-from PySide6.QtCore import QObject, QDir, QFileInfo
+
+import scipy
+from PySide6.QtCore import QObject, QDir, QFileInfo, SLOT
 from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout
+
 import pydub as pd
+import pyloudnorm as pyln
+import numpy as np
+
 import music_tag as tag
 from filetags import FileTags
 from export_params import ExportParameters, valid_extensions
@@ -11,9 +18,14 @@ class FileInfo(QObject):
     absolute_path = ""
     relative_path = ""
     file_info: QFileInfo
+    extension: str = ""
     original_tags: FileTags
     desired_tags: FileTags
+    # Audio processing related
     loudness: Optional[float] = None
+    sample_rate: Optional[int] = None
+    _audio_segment: Optional[pd.AudioSegment] = None
+    _audio_array: Optional[np.ndarray] = None
 
     def __init__(self, parent: "FileInfoWidget", absolute_path, relative_path):
         super().__init__(parent)
@@ -21,18 +33,52 @@ class FileInfo(QObject):
         self.relative_path = relative_path
         self.file_info = QFileInfo(absolute_path)
         assert self.file_info.isFile() and self.file_info.completeSuffix() in valid_extensions
+        self.extension = self.file_info.completeSuffix()
         self.original_tags = FileTags(tag.load_file(self.absolute_path))
         self.desired_tags = FileTags(tag.load_file(self.absolute_path))
 
+    # Methods for loading audio data
+    def loadAudioSegment(self):
+        if self._audio_segment is None:
+            self._audio_segment = pd.AudioSegment.from_file(self.absolute_path, self.extension)
+            self.sample_rate = self._audio_segment.frame_rate
+
+    def loadAudioArray(self):
+        if self._audio_array is None:
+            self.loadAudioSegment()
+            data = np.array(self._audio_segment.get_array_of_samples())[::self._audio_segment.channels]
+            data = data.astype(np.float32) / 10000
+            data -= data.mean()
+            self._audio_array = pyln.normalize.peak(data, -1.0)
+
+    def clearAudioData(self):
+        self._audio_segment = None
+        self._audio_array = None
 
     def measureLoudness(self):
-        # Implementacja funkcji measureLoudness
-        print("Measuring loudness...")
-        self.loudness = -12.0
+        self.loadAudioArray()
+        meter = pyln.Meter(self.sample_rate)
+        self.loudness = meter.integrated_loudness(self._audio_array)
 
-    def export(self, export_params: ExportParameters):
+    def onlyMeasureLoudness(self) -> None:
+        """
+        Measure loudness and clear the loaded audio data
+        """
+        self.measureLoudness()
+        self.clearAudioData()
+
+    def normalizeLoudness(self, target_loudness: float = -23.0):
+        self.measureLoudness()
+        self._audio_array = pyln.normalize.loudness(self._audio_array, self.loudness, target_loudness)
+        # Convert back to AudioSegment
+        wav_io = io.BytesIO()
+        scipy.io.wavfile.write(wav_io, self.sample_rate, self._audio_array)
+        wav_io.seek(0)
+        self._audio_segment = pd.AudioSegment.from_wav(wav_io)
+
+    def export(self, params: ExportParameters):
         # Implementacja funkcji export
-        print(f"Exporting file with parameters: {export_params}")
+        print(f"Exporting file with parameters: {params}")
 
 
 class FileInfoWidget(QWidget):
@@ -59,4 +105,3 @@ class FileInfoWidget(QWidget):
             self.loudness_label.setText(f"Not measured")
         else:
             self.loudness_label.setText(f"{self.file_info.loudness} LUFS")
-
--- tests/loading_test.py
+++ tests/loading_test.py
@@ -1,7 +1,7 @@
 import os
 from PySide6.QtWidgets import QApplication
 from filelist import FileList
-
+from fileinfo import FileInfo, FileInfoWidget
 
 def testLoading():
     filepath = os.path.dirname(os.path.abspath(__file__)) + "/audio-src/test.mp3"
@@ -14,6 +14,18 @@ def testLoading():
         print("Album: " + flist.files[0].file_info.original_tags.tags["album"])
 
 
+def testNormalization():
+    widget = FileInfoWidget(None, os.getcwd()+"/audio-src/test.mp3", "")
+    file = widget.file_info
+    print(file.loudness)
+    file.measureLoudness()
+    print(file.loudness)
+    file.normalizeLoudness(-21)
+    file.measureLoudness()
+    print(file.loudness)
+
+
 if __name__ == "__main__":
     app = QApplication()
     testLoading()
+    testNormalization()
